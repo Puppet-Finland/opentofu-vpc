b279a3453d181f56ce6355cb82e97c9d3eb4e823
Samuli Seppänen <samuli.seppanen@puppeteers.net>
2022-07-06 14:14:36 +0300

    Add documentation
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.net>

---
+++ README.md
@@ -0,0 +1,23 @@
+# terraform-vpc
+
+This is a module for creating AWS VPC using a standard pattern:
+
+* Two public subnets
+* Two private subnets
+    * IPv4 egress via NAT gateway (extra cost)
+    * IPv6 egress via egress-only internet gateway
+
+Example usage:
+
+    module "acme-vpc" {
+      source                              = <git-url>
+      basename                            = "acme"
+      region                              = "us-west-1"
+      vpc_cidr_block                      = "10.172.0.0/20"
+      primary_public_subnet_cidr_block    = "10.172.0.0/24"
+      secondary_public_subnet_cidr_block  = "10.172.1.0/24"
+      primary_private_subnet_cidr_block   = "10.172.2.0/24"
+      secondary_private_subnet_cidr_block = "10.172.3.0/24"
+    }
+
+See [input.tf](input.tf) for input variable documentation.
--- input.tf
+++ input.tf
@@ -1,28 +1,35 @@
-# Base name for names, tags, etc.
 variable "basename" {
-  type = string
+  description = "Prefix or suffix to add to all resource names"
+  type        = string
 }
 
 variable "region" {
-  type = string
+  description = "AWS region to create the VPC in"
+  type        = string
 }
 
 variable "vpc_cidr_block" {
-  type = string
+  description = "CIDR block for the VPC"
+  type        = string
 }
 
 variable "primary_public_subnet_cidr_block" {
-  type = string
+  description = "CIDR block for the primary public subnet"
+  type        = string
 }
 
 variable "secondary_public_subnet_cidr_block" {
-  type = string
+  description = "CIDR block for the secondary public subnet"
+  type        = string
+
 }
 
 variable "primary_private_subnet_cidr_block" {
-  type = string
+  description = "CIDR block for the primary private subnet"
+  type        = string
 }
 
 variable "secondary_private_subnet_cidr_block" {
-  type = string
+  description = "CIDR block for the secondary private subnet"
+  type        = string
 }
